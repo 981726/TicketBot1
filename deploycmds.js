6766aa97e7df6ee1304b2c495824fee30f169586
X <109253617+981726@users.noreply.github.com>
2025-04-17 23:54:30 -0700

    Update deploycmds.js

--- deploycmds.js
+++ deploycmds.js
@@ -1,11 +1,9 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-// Define your slash commands using SlashCommandBuilder
 const setupPanelCommand = new SlashCommandBuilder()
     .setName('setuppanel')
     .setDescription('Sets up the payment panel.');
 
-// Export the array of slash commands
 module.exports = [
     setupPanelCommand
 ];
